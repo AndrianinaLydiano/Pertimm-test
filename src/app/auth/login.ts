c8c1317ef05ff4efd69db4e4814b1d2bba7abef9
lydiano <andrianina.lydiano@gmail.com>
2025-06-25 08:22:37 +0300

    add login component with form handling and validation

---
+++ src/app/auth/login.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { ToastrService } from 'ngx-toastr'
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, RouterLink],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  private authService = inject(AuthService);
+  private toastr = inject(ToastrService);
+
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    if (!this.email || !this.password) {
+      this.toastr.error('Veuillez remplir tous les champs.');
+      return;
+    }
+
+    this.authService.login({ email: this.email, password: this.password }).subscribe({
+      next: () => {
+        this.toastr.success(`Bienvenue ${this.email}`, 'Success');
+      },
+      error: (error) => {
+        if (error.status === 401) {
+          this.toastr.error('Identifiants incorrects, veuillez réessayer.', 'Error');
+        } else {
+          this.toastr.error('Erreur lors du login: ' + error.message, 'Error');
+        }
+      }
+    });
+  }
+}
