84700deaa66e7681cf9503ba3a8ec588fea18226
lydiano <andrianina.lydiano@gmail.com>
2025-06-25 08:22:51 +0300

    set up routing with login and registration paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login';
+import { Register } from './auth/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+];
